f04b7e2c00b03eadef88bf77fa3e7b8ad2950f0e
djakimiuk <djakimiuk@outlook.com>
2023-07-18 20:07:11 +0200

    ex. 4.1 Blog list, step1 solved

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
-})
+  likes: Number,
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+const mongoUrl = process.env.MONGODB_URI;
 
-app.use(cors())
-app.use(express.json())
+mongoose
+  .connect(mongoUrl)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(`error connecting to MongoDB: ${error.message}`);
+  });
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use(cors());
+app.use(express.json());
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+morgan.token('body', (request, response) => JSON.stringify(request.body))
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(
+    morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
-const PORT = 3003
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
