bb90f0e5ce362ad11716049ee7ad28de1c6b5e70
djakimiuk <63128911+djakimiuk@users.noreply.github.com>
2023-07-20 22:55:13 +0200

    ex. 4.2 Blog list, step2 solved

--- index.js
+++ index.js
@@ -1,54 +1,7 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
+const app = require("./app");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = process.env.MONGODB_URI;
-
-mongoose
-  .connect(mongoUrl)
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log(`error connecting to MongoDB: ${error.message}`);
-  });
-
-app.use(cors());
-app.use(express.json());
-
-morgan.token('body', (request, response) => JSON.stringify(request.body))
-
-app.use(
-    morgan(':method :url :status :res[content-length] - :response-time ms :body')
-)
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = process.env.PORT || 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
