a1876392cc80b232b5b6750b559a802d686846a9
djakimiuk <63128911+djakimiuk@users.noreply.github.com>
2023-07-24 23:57:21 +0200

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,4 +9,13 @@ const totalLikes = (blogs) => {
 
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
-module.exports = { dummy, totalLikes };
+
+const favoriteBlog = (blogs) => {
+  const blogsCopy = [...blogs].sort((a, b) => b.likes - a.likes);
+  const result = blogsCopy[0];
+  delete result._id;
+  delete result.url;
+  delete result.__v;
+  return result;
+};
+module.exports = { dummy, totalLikes, favoriteBlog };
